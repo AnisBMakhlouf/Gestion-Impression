19af8106ea380c270c4dce6d56b90ebecab01122
Anis Benmakhlouf <anis.benmakhlouf@progedsolutions.com>
2022-06-09 22:39:00 +0100

    AdminDAO @ Controller

---
+++ src/main/java/controllers/AdministrateurController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javax.servlet.ServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import dao.AdministrateurDAO;
+import models.Administrateur;
+
+public class AdministrateurController {
+
+
+}
