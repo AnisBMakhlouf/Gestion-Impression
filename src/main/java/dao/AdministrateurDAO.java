06360afbf44d6e108c607088d20c9533bcdb7d12
Anis Benmakhlouf <anis.benmakhlouf@progedsolutions.com>
2022-06-10 11:16:23 +0100

    AdminDAO @ Controller

--- src/main/java/dao/AdministrateurDAO.java
+++ src/main/java/dao/AdministrateurDAO.java
@@ -1,5 +1,25 @@
 package dao;
+import java.sql.ResultSet;
 
+import java.sql.SQLException;
+import models.Administrateur;
+
+import util.JDBCUtil;
 public class AdministrateurDAO {
+	
 
+	public static Administrateur FindByLoginPwd(String log,String pwd) throws SQLException {
+		Administrateur u = null;
+		String query ="Select * from administrateur where login = '"+log+"' and password ='"+pwd+"'";
+		ResultSet rs;
+		try {
+			rs = JDBCUtil.getStatement().executeQuery(query);
+			if(rs.next()) {
+				u=new Administrateur(log, pwd, 0);
+			}
+} catch (SQLException e) {
+// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+		return u;}
 }
